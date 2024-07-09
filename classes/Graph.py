999b057b698f21dece1ae4903e3e8c97e101f83c
Julius Rückin <julius.rueckin@gmail.com>
2024-07-09 16:37:37 +0200

    add MIT license to all python module derived from CatNIPP's codebase

--- classes/Graph.py
+++ classes/Graph.py
@@ -1,6 +1,30 @@
 # Adapted from https://gist.github.com/betandr/541a1f6466b6855471de5ca30b74cb31
 from decimal import Decimal
 
+"""
+MIT License from https://github.com/marmotlab/CAtNIPP/
+
+Copyright (c) 2022 MARMot Lab @ NUS-ME
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
 
 class Edge:
     def __init__(self, to_node, length):
